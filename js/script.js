58b48f6d61bdf85b0851824b6b72af2bf48f4ca7
Hernan Mendez <hernan21112001@gmail.com>
2017-07-03 11:07:06 -0400

    ROOT

---
+++ js/script.js
@@ -0,0 +1,7 @@
+d3.json('https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json',function(error,data){
+if(error){
+    alert('OOPS! something happened, please reload the website')
+    return '';
+}
+
+});
