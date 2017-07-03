2b7df963b6135bbc1c07eb3879f5a3b7f9ac94ee
Hernan Mendez <hernan21112001@gmail.com>
2017-07-03 11:42:43 -0400

    Setting initial values and CSS

--- js/script.js
+++ js/script.js
@@ -3,5 +3,31 @@ if(error){
     alert('OOPS! something happened, please reload the website')
     return '';
 }
+var width = 900;
+var height = 900;
+var nodes = data.nodes;
+var links = data.links;
 
+d3.select('#main').style('width',width+'px').style('height',height+'px');
+
+var force = d3.layout.force()
+    .size([width,height])
+    .nodes(nodes)
+    .links(links)
+    .on('tick',tick)
+    .linkDistance(80)
+    .charge([-100])
+
+var svg = d3.select('#main').append('svg')
+    .attr('width', width)
+    .attr('height', height);
+    
+var link = svg.selectAll('.link')
+    .data(links)
+    .enter().append('line')
+    .attr('class', 'link');
+
+function tick(){
+    
+}
 });
